06ff1c9fe059aa611c1270925daaccf9f775245d
Tim Thayne <timthayne@gmail.com>
2017-10-30 11:17:16 -0600

    Periodic check-in

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -1,8 +1,16 @@
 var t = 50;
 var s = 10;
+var f = getWindChillFactor(t, s);
 
-var f = 100; //TODO:
-
+document.getElementById("windSpeed").innerHTML = s;
+document.getElementById("temp").innerHTML = t;
 document.getElementById("windChillFactor").innerHTML = f;
 
 console.log(f);
+
+function getWindChillFactor(t, s)
+{
+    //TODO:
+    return 110;
+}
+
