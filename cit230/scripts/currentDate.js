a53cc537df7d2e0a38732b5b7a89156dc9966a0a
timthayne <timthayne@gmail.com>
2019-09-23 10:51:45 -0600

    Added dynamic year to footer

---
+++ cit230/scripts/currentDate.js
@@ -0,0 +1,9 @@
+var currentDate = new Date();
+console.log(currentDate);
+
+var currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+document.getElementById("currentYear").innerHTML = currentYear;
+
+
