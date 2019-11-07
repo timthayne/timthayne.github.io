57f3839ca225fa798c66510131f5762fc856d9fa
Tim Thayne <timthayne@gmail.com>
2019-11-06 18:26:05 -0700

    Added CIT 230 files

--- cit230/lesson8/scripts/main.js
+++ cit230/lesson8/scripts/main.js
@@ -44,30 +44,3 @@ currentDateString += ' ' + month;
 currentDateString += ' ' + currentDate.getFullYear();
 
 document.getElementById('currentDate').innerHTML = currentDateString;
-
-
-// create the five-day forecast
-let currentDay = weekDayNumber;
-
-for (let i = 1; i < 6; i++) {
-    // add one day to current day
-    currentDay++;
-
-    // if current day is greater than 6 (Saturday), reset it to 0 (Sunday)
-    if (currentDay > 6) {
-        currentDay = 0;
-    }
-
-    // assign value to placeholder in HTML
-    const element = document.getElementById(`day${i}`);
-
-    element.innerHTML = daysOfWeek[currentDay];
-}
-
-// show/hide pancake
-if (weekDayNumber === 5) {
-    document.getElementById("pancake")
-        .removeAttribute("class", "hidden");
-}
-
-
